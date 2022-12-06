a58185a92500e18b4352b96062de3e65dd06a351
REBECCA YU <rebeccayu2015@gmail.com>
2022-12-05 23:13:53 -0500

    Removed password stuff

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Item(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=100)
+    curr_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='curr')
+    claim_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='claim')
+    where_found = models.CharField(max_length=100)
+    where_left = models.CharField(max_length=100)
+    time_found = models.DateTimeField(auto_now_add=True)
+    time_claimed = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=50)
--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('sign-up/', views.sign_up, name='sign_up'),
     path('login/', views.MyLoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', views.logout, name='logout'),
-    path('reset-password/', views.MyPasswordResetView.as_view(), name='reset_password'),
-    path('reset-password/sent/', views.MyPasswordResetDoneView.as_view(), name='reset_password_sent'),
-    path('reset-password/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='reset_password_confirm'),
-    path('reset-password/done/', views.MyPasswordResetCompleteView.as_view(), name='reset_password_complete'),
-    path('change-password/', views.MyPasswordChangeView.as_view(), name='change_password'),
-    path('change-password/done/', views.MyPasswordChangeCompleteView.as_view(), name='change_password_complete'),
+    # path('reset-password/', views.MyPasswordResetView.as_view(), name='reset_password'),
+    # path('reset-password/sent/', views.MyPasswordResetDoneView.as_view(), name='reset_password_sent'),
+    # path('reset-password/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='reset_password_confirm'),
+    # path('reset-password/done/', views.MyPasswordResetCompleteView.as_view(), name='reset_password_complete'),
+    # path('change-password/', views.MyPasswordChangeView.as_view(), name='change_password'),
+    # path('change-password/done/', views.MyPasswordChangeCompleteView.as_view(), name='change_password_complete'),
     path('profile/', views.profile, name='profile')
 ]
