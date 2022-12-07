11a455542b4e02ad48bf25a08c79c5ac8c1a356b
REBECCA YU <rebeccayu2015@gmail.com>
2022-12-06 22:16:16 -0500

    Added notifs, submissions, and delete

--- main/views.py
+++ main/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from main.models import Item
 from datetime import datetime
 from django.utils import timezone
+from django.contrib import messages
 
 # Create your views here.
 @login_required(login_url='/login/')
 def item_list(request):
     items = Item.objects.filter(status="Claimed") | Item.objects.filter(status="Unclaimed")
 
-    item = Item(item_name="Crepe Note 1",
-                curr_user=request.user,
-                where_found="ur dad's butt",
-                where_left="ur mom's bedroom",
-                description="badksfasdfasdfksadfasdfasdfasdfasdf",
-                status='Unclaimed')
-    item.save()
+    # item = Item(item_name="Sample Item",
+    #             curr_user=request.user,
+    #             where_found="Cafeteria",
+    #             where_left="Front Office",
+    #             description="Sample description",
+    #             status='Unclaimed')
+    # item.save()
 
     # "Delete" after 15 seconds (practically 1 day)
     for item in Item.objects.filter(status="Claimed"):
@@ -32,16 +33,24 @@ def item_list(request):
         id = request.POST.get('item-id')
         item = Item.objects.filter(item_id=id).first()
         
-        if item.status == "Unclaimed":
+        if item.status == "Unclaimed" and item.curr_user == request.user:
+            item.delete()
+            messages.warning(request, "Deleted item \"" + item.item_name + "\".")
+            return redirect('item_list')
+        elif item.status == "Unclaimed":
             item.claim_user = request.user
             item.time_claimed = datetime.now()
             item.status = "Claimed"
             item.save()
+            messages.success(request, "Claimed item \"" + item.item_name + "\".")
+            return redirect('item_list')
         elif item.status == "Claimed":
             item.claim_user = None
             item.time_claimed = None
             item.status = "Unclaimed"
             item.save()
+            messages.warning(request, "Unclaimed item \"" + item.item_name + "\".")
+            return redirect('item_list')
 
     user = request.user
     context = {
@@ -69,5 +78,7 @@ def add_item(request):
                     image=item_image,
                     status='Unclaimed')
         item.save()
+        messages.success(request, "Added item \"" + item.item_name + "\".")
+        return redirect('add_item')
 
     return render(request, 'main/add_item.html')
